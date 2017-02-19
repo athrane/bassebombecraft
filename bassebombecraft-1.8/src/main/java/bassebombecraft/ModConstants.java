7f21f822c07e8a3f27170ae4bdccc641e33e3b57
Allan Thrane Andersen <einheriii@gmail.com>
2017-02-19 20:54:47 +0100

    Update mod version info to 1.11-1.24, closes #124

--- bassebombecraft-1.8/src/main/java/bassebombecraft/ModConstants.java
+++ bassebombecraft-1.8/src/main/java/bassebombecraft/ModConstants.java
@@ -21,7 +21,7 @@ public class ModConstants {
     /**
      * Mod version.
      */
-    public static final String VERSION = "1.11-1.23";
+    public static final String VERSION = "1.11-1.24";
     
     /**
      * In game tab name.
