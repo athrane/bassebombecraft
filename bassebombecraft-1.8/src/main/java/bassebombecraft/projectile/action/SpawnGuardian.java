f10d40b3d1630a4fd290e79a9e3d432bd8838300
Allan Thrane Andersen <einheriii@gmail.com>
2017-02-27 22:10:01 +0100

    Entities spawned by book of guardian is member of player team, closes #140.

--- bassebombecraft-1.8/src/main/java/bassebombecraft/projectile/action/SpawnGuardian.java
+++ bassebombecraft-1.8/src/main/java/bassebombecraft/projectile/action/SpawnGuardian.java
@@ -1,8 +1,10 @@
 package bassebombecraft.projectile.action;
 
+import static bassebombecraft.BassebombeCraft.getBassebombeCraft;
 import static bassebombecraft.entity.ai.AiUtils.buildCharmedMobAi;
 import static bassebombecraft.entity.ai.AiUtils.clearAiTasks;
 
+import bassebombecraft.event.entity.team.TeamRepository;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.monster.EntityIronGolem;
 import net.minecraft.entity.projectile.EntityThrowable;
@@ -22,6 +24,12 @@ public class SpawnGuardian implements ProjectileAction {
 		EntityIronGolem entity= new EntityIronGolem(world);
 		entity.setLocationAndAngles(projectile.posX, projectile.posY, projectile.posZ, projectile.rotationYaw, projectile.rotationPitch);
 		world.spawnEntity(entity);
+		
+		// add entity to team
+		TeamRepository teamRepository = getBassebombeCraft().getTeamRepository();
+		teamRepository.add(entity, owner.getName());
+
+		// set AI		
 		clearAiTasks(entity);
 		buildCharmedMobAi(entity, owner);		
 	}
