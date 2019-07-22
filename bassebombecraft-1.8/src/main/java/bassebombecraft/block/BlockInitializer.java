060173ff83c8a7c95757f9a80a8332636c6e8e7a
Thrane <einheriii@gmail.com>
2019-07-22 17:39:05 +0200

    1.14.3 build fails with error: cannot find symbol void registerBlock(CreativeTabs targetTab, ..., closes #373

--- bassebombecraft-1.8/src/main/java/bassebombecraft/block/BlockInitializer.java
+++ bassebombecraft-1.8/src/main/java/bassebombecraft/block/BlockInitializer.java
@@ -1,51 +1,24 @@
 package bassebombecraft.block;
 
-import static bassebombecraft.BassebombeCraft.getProxy;
-
 import net.minecraft.block.Block;
-import net.minecraft.block.material.Material;
-import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.item.ItemBlock;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 
 /**
  * Class for initializing blocks.
  */
+@Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
 public class BlockInitializer {
-
+	
 	/**
 	 * Initialize blocks.
 	 * 
-	 * @param targetTab
-	 *            tab that item is added to.
-	 */
-	public void initialize(CreativeTabs targetTab) {
-
-		RandomBookBlock randomBookBlock = new RandomBookBlock(Material.GROUND);
-		registerBlock(targetTab, randomBookBlock, RandomBookBlock.BLOCK_NAME);
-	}
-
-	/**
-	 * Helper method for registration of block.
-	 * 
-	 * @param targetTab
-	 *            target tab for block.
-	 * @param block
-	 *            block to register.
-	 * @param blockName
-	 *            block name.
-	 */
-	void registerBlock(CreativeTabs targetTab, Block block, String blockName) {
-		block.setCreativeTab(targetTab);
-		block.setRegistryName(blockName);
-		//GameRegistry.register(block);
-		ItemBlock itemBlock = new ItemBlock(block);
-		itemBlock.setRegistryName(blockName);
-		//GameRegistry.register(itemBlock);
-		getProxy().registerItemForRendering(block, blockName);
-	}
-
-	public static BlockInitializer getInstance() {
-		return new BlockInitializer();
-	}
-
+	 * @param event register blocks event.
+	 */	
+    @SubscribeEvent
+    public static void onBlocksRegistry(RegistryEvent.Register<Block> event) {
+        event.getRegistry().register(new RandomBookBlock());
+    }
+    
 }
