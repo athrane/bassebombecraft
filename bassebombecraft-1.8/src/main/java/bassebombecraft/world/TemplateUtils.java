98d7dcf5cdb4a1c18c09cb95a95256c83757d175
Thrane <einheriii@gmail.com>
2019-08-11 21:10:56 +0200

    1.14.3 build fails with error: StructureBoundingBox cannot be resolved to a type, closes #430

--- bassebombecraft-1.8/src/main/java/bassebombecraft/world/TemplateUtils.java
+++ bassebombecraft-1.8/src/main/java/bassebombecraft/world/TemplateUtils.java
@@ -8,15 +8,14 @@ import org.apache.commons.lang3.RandomStringUtils;
 import bassebombecraft.BassebombeCraft;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
-import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MutableBoundingBox;
+import net.minecraft.world.ServerWorld;
 import net.minecraft.world.World;
-import net.minecraft.world.WorldServer;
 import net.minecraft.world.gen.feature.template.PlacementSettings;
 import net.minecraft.world.gen.feature.template.Template;
 import net.minecraft.world.gen.feature.template.TemplateManager;
-import net.minecraft.world.gen.structure.StructureBoundingBox;
 
 /**
  * Helper class for using templates
@@ -31,14 +30,12 @@ public class TemplateUtils {
 	/**
 	 * Save structure as template.
 	 * 
-	 * @param world
-	 *            world object.
-	 * @param offset
-	 *            world structure offset.
-	 * @param size
-	 *            structure size.
-	 *            
-	 * @return resource location where structure was saved. is null if at client side.
+	 * @param world  world object.
+	 * @param offset world structure offset.
+	 * @param size   structure size.
+	 * 
+	 * @return resource location where structure was saved. is null if at client
+	 *         side.
 	 */
 	public static ResourceLocation save(World world, BlockPos offset, BlockPos size) {
 
@@ -49,22 +46,19 @@ public class TemplateUtils {
 		String templateName = new StringBuilder().append(MODID).append("-").append(VERSION).append("-")
 				.append(RandomStringUtils.randomAlphabetic(10)).toString();
 
-		// get server
-		MinecraftServer minecraftserver = world.getMinecraftServer();
-
 		// get template manager
-		WorldServer worldserver = (WorldServer) world;
+		ServerWorld worldserver = (ServerWorld) world;
 		TemplateManager templatemanager = worldserver.getStructureTemplateManager();
 
 		// create template
 		ResourceLocation location = new ResourceLocation(templateName);
-		Template template = templatemanager.getTemplate(minecraftserver, location);
+		Template template = templatemanager.getTemplate(location);
 		template.takeBlocksFromWorld(world, offset, size, DONT_TAKE_ENTITIES, Blocks.STRUCTURE_VOID);
 		String author = BassebombeCraft.getBassebombeCraft().getUser();
 		template.setAuthor(author);
-				
+
 		// save template
-		templatemanager.writeTemplate(minecraftserver, location);
+		templatemanager.writeToFile(location);
 
 		return location;
 	}
@@ -72,12 +66,9 @@ public class TemplateUtils {
 	/**
 	 * Load structure from template.
 	 * 
-	 * @param world
-	 *            world object.
-	 * @param name
-	 *            structure name to load.
-	 * @param offset
-	 *            world offset where structure should be placed.
+	 * @param world  world object.
+	 * @param name   structure name to load.
+	 * @param offset world offset where structure should be placed.
 	 */
 	public static void load(World world, String name, BlockPos offset) {
 
@@ -85,15 +76,12 @@ public class TemplateUtils {
 		if (WorldUtils.isWorldAtClientSide(world))
 			return;
 
-		// get server
-		MinecraftServer minecraftserver = world.getMinecraftServer();
-
 		// get template manager
-		WorldServer worldserver = (WorldServer) world;
+		ServerWorld worldserver = (ServerWorld) world;
 		TemplateManager templatemanager = worldserver.getStructureTemplateManager();
 
 		// get template
-		Template template = templatemanager.getTemplate(minecraftserver, new ResourceLocation(name));
+		Template template = templatemanager.getTemplate(new ResourceLocation(name));
 
 		// exit if template is undefined
 		if (template == null) {
@@ -109,9 +97,9 @@ public class TemplateUtils {
 
 		// define bounding box
 		BlockPos boundingBoxSize = offset.add(template.getSize());
-		
-		StructureBoundingBox boundingBox = new StructureBoundingBox(offset, boundingBoxSize);
-		
+
+		MutableBoundingBox boundingBox = new MutableBoundingBox(offset, boundingBoxSize);
+
 		// create placement settings
 		PlacementSettings placementsettings = new PlacementSettings().setBoundingBox(boundingBox);
 
