4f3298f19d30a13ebe4056e88c83a3b8d7ac9e2b
Thrane <einheriii@gmail.com>
2019-08-11 20:44:35 +0200

    1.14.3 build fails with error: net.minecraft.client.renderer.entity.RenderManager cannot be resolved, closes #429

--- bassebombecraft-1.8/src/main/java/bassebombecraft/rendering/RenderingUtils.java
+++ bassebombecraft-1.8/src/main/java/bassebombecraft/rendering/RenderingUtils.java
@@ -3,7 +3,7 @@ package bassebombecraft.rendering;
 import com.mojang.blaze3d.platform.GlStateManager;
 
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.client.renderer.entity.EntityRendererManager;
 import net.minecraft.entity.Entity;
 
 /**
@@ -15,40 +15,40 @@ public class RenderingUtils {
 	 * Setup billboard rotation to face camera.
 	 */
 	public static void setupBillboardRotation() {
-		final Minecraft mc = Minecraft.getMinecraft();		
-		RenderManager renderManager = mc.getRenderManager();
-		GlStateManager.rotate(-renderManager.playerViewY, 0.0F, 1.0F, 0.0F);
-		GlStateManager.rotate(renderManager.playerViewX, 1.0F, 0.0F, 0.0F);
-	}		
-	
+		final Minecraft mc = Minecraft.getInstance();
+		EntityRendererManager renderManager = mc.getRenderManager();
+		GlStateManager.rotatef(-renderManager.playerViewY, 0.0F, 1.0F, 0.0F);
+		GlStateManager.rotatef(renderManager.playerViewX, 1.0F, 0.0F, 0.0F);
+	}
+
 	/**
 	 * Setup billboard rotation to face camera.
-	 */	
+	 */
 	public static void setupBillboardRotationV2() {
-		final Minecraft mc = Minecraft.getMinecraft();		
-	    final Entity rve = mc.getRenderViewEntity();
-		GlStateManager.rotate(-rve.rotationYaw, 0, 1, 0);
-		GlStateManager.rotate(rve.rotationPitch, 1, 0, 0);
+		final Minecraft mc = Minecraft.getInstance();
+		final Entity rve = mc.getRenderViewEntity();
+		GlStateManager.rotatef(-rve.rotationYaw, 0, 1, 0);
+		GlStateManager.rotatef(rve.rotationPitch, 1, 0, 0);
 	}
 
 	/**
 	 * Setup billboard rendering options.
 	 */
 	public static void resetBillboardRendering() {
-		GlStateManager.enableTexture2D();
+		GlStateManager.enableTexture();
 		GlStateManager.enableLighting();
-		GlStateManager.enableDepth();		
+		GlStateManager.enableDepthTest();;
 		GlStateManager.popMatrix();
 	}
 
 	/**
 	 * reset billboard rendering options.
-	 */	
+	 */
 	public static void setupBillboardRendering() {
 		GlStateManager.pushMatrix();
 		GlStateManager.disableLighting();
-		GlStateManager.disableTexture2D();
-		GlStateManager.disableDepth();
-	}	
-	
+		GlStateManager.disableTexture();
+		GlStateManager.disableDepthTest();
+	}
+
 }
