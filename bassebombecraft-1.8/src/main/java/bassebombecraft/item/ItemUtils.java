6612250ab1428c244d85d1ba8a10551e11e0614f
Thrane <einheriii@gmail.com>
2019-08-11 21:45:10 +0200

    1.14.3 build fails with error: The method setUnlocalizedName(String) is undefined for the type Item, closes #432

--- bassebombecraft-1.8/src/main/java/bassebombecraft/item/ItemUtils.java
+++ bassebombecraft-1.8/src/main/java/bassebombecraft/item/ItemUtils.java
@@ -17,7 +17,6 @@ public class ItemUtils {
 	 *            item name.
 	 */
 	public static void doCommonItemInitialization(Item item, String name) {		
-		item.setUnlocalizedName(name);
 		item.setRegistryName(name);
 	}
 
