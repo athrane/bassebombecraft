53e465162cbbb863ff8a8ce20ab6507e7e600764
Thrane <einheriii@gmail.com>
2019-09-17 13:40:15 +0200

    HUD item renders information about team members, closes #500

--- bassebombecraft-1.8/src/main/java/bassebombecraft/rendering/RenderingUtils.java
+++ bassebombecraft-1.8/src/main/java/bassebombecraft/rendering/RenderingUtils.java
@@ -2,8 +2,10 @@ package bassebombecraft.rendering;
 
 import static bassebombecraft.ModConstants.BILLBOARD_LINE_WIDTH;
 import static bassebombecraft.ModConstants.EQUILATERAL_TRIANGLE_HEIGHT;
+import static bassebombecraft.ModConstants.TEXT_BILLBOARD_ROTATION;
 import static bassebombecraft.ModConstants.TEXT_COLOR;
 import static bassebombecraft.ModConstants.TEXT_SCALE;
+import static net.minecraftforge.fml.common.ObfuscationReflectionHelper.getPrivateValue;
 
 import javax.vecmath.Vector4f;
 
@@ -23,6 +25,27 @@ import net.minecraft.util.math.Vec3d;
  * Rendering utilities.
  */
 public class RenderingUtils {
+
+	/**
+	 * Get render position from {@linkplain EntityRendererManager}.
+	 * 
+	 * The value are private fields which are accessed using reflection.
+	 * 
+	 * @return render position.
+	 */
+	public static Vec3d getRenderPos() {
+
+		// get render manager
+		EntityRendererManager renderManager = Minecraft.getInstance().getRenderManager();
+
+		// get value from private fields
+		double renderPosX = getPrivateValue(EntityRendererManager.class, renderManager, "field_78725_b");
+		double renderPosY = getPrivateValue(EntityRendererManager.class, renderManager, "field_78726_c");
+		double renderPosZ = getPrivateValue(EntityRendererManager.class, renderManager, "field_78723_d");
+		Vec3d renderPos = new Vec3d(renderPosX, renderPosY, renderPosZ);
+
+		return renderPos;
+	}
 
 	/**
 	 * Setup billboard rotation to face camera.
@@ -48,20 +71,29 @@ public class RenderingUtils {
 	 * Setup billboard rendering options.
 	 */
 	public static void resetBillboardRendering() {
+
+		// get matrix
+		GlStateManager.popMatrix();
+
+		// set attribute
 		GlStateManager.enableTexture();
 		GlStateManager.enableLighting();
 		GlStateManager.enableDepthTest();
-		GlStateManager.popMatrix();
 	}
 
 	/**
 	 * reset billboard rendering options.
 	 */
 	public static void setupBillboardRendering() {
+
+		// save matrix
 		GlStateManager.pushMatrix();
+
 		GlStateManager.disableLighting();
 		GlStateManager.disableTexture();
 		GlStateManager.disableDepthTest();
+
+		// GlStateManager.pushMatrix();
 	}
 
 	/**
@@ -237,10 +269,10 @@ public class RenderingUtils {
 	 * Render text at origin.
 	 * 
 	 * @param translation translation vector.
-	 * @param text      text to render
-	 * @param rotation  rotation
+	 * @param text        text to render
+	 * @param rotation    rotation
 	 */
-	public static void renderTextBillboard(Vec3d translation, String text, Vector4f rotation) {
+	public static void renderTextBillboardV2(Vec3d translation, String text, Vector4f rotation) {
 		setupBillboardRendering();
 
 		// get minecraft
@@ -252,14 +284,8 @@ public class RenderingUtils {
 		// translate to camera position
 		GlStateManager.translated(translation.x, translation.y, translation.z);
 
-		GL11.glNormal3f(0.0F, 1.0F, 0.0F);
-		
 		// set up billboard rotation
-		//setupBillboardRotation();
-		EntityRendererManager renderManager = mc.getRenderManager();
-		GlStateManager.rotatef(-renderManager.playerViewY, 0.0F, 1.0F, 0.0F);
-		GlStateManager.rotatef(renderManager.playerViewX, 1.0F, 0.0F, 0.0F);
-		
+		setupBillboardRotation();
 
 		// scale text
 		GlStateManager.scalef(TEXT_SCALE, TEXT_SCALE, TEXT_SCALE);
@@ -271,5 +297,147 @@ public class RenderingUtils {
 		mc.fontRenderer.drawString(text, 0, 0, TEXT_COLOR);
 
 		resetBillboardRendering();
-	}	
+	}
+
+	/**
+	 * Render text at origin for rendering of HUD text.
+	 * 
+	 * This method supports translation of the text relative to the player view
+	 * direction and independent of the camera (or player) orientation and
+	 * placement.
+	 * 
+	 * @param cameraTranslation camera translation vector.
+	 * @param textTranslation   text translation vector for translation of text
+	 *                          relative to view direction. Defines the placement of
+	 *                          the HUD text.
+	 * @param text              text to render
+	 */
+	public static void renderHudTextBillboard(Vec3d cameraTranslation, Vec3d textTranslation, String text) {
+		setupBillboardRendering();
+
+		// get minecraft
+		Minecraft mc = Minecraft.getInstance();
+
+		// enable for rendering of text
+		GlStateManager.enableTexture();
+
+		// translate to camera position
+		GlStateManager.translated(cameraTranslation.x, cameraTranslation.y, cameraTranslation.z);
+
+		// set up billboard rotation
+		setupBillboardRotation();
+
+		// translation of text relative to view direction.
+		// Defines the placement of the HUD text
+		GlStateManager.translated(textTranslation.x, textTranslation.y, textTranslation.z);
+
+		// scale text
+		GlStateManager.scalef(TEXT_SCALE, TEXT_SCALE, TEXT_SCALE);
+
+		// add billboard rotation for text readability
+		GlStateManager.rotatef(TEXT_BILLBOARD_ROTATION.w, TEXT_BILLBOARD_ROTATION.x, TEXT_BILLBOARD_ROTATION.y,
+				TEXT_BILLBOARD_ROTATION.z);
+
+		// draw
+		mc.fontRenderer.drawString(text, 0, 0, TEXT_COLOR);
+
+		resetBillboardRendering();
+	}
+
+	/**
+	 * Render rotated text at origin for rendering compass.
+	 * 
+	 * This method supports rotation of the text relative to the player view
+	 * direction and independent of the camera (or) player orientation and
+	 * placement. *
+	 * 
+	 * @param cameraTranslation camera translation vector.
+	 * @param text              text to render
+	 * @param rotation          rotation rotation vector for rotation of text
+	 *                          relative to view direction.
+	 */
+	public static void renderRotatedTextBillboard(Vec3d cameraTranslation, Vector4f rotation, String text) {
+		setupBillboardRendering();
+
+		// get minecraft
+		Minecraft mc = Minecraft.getInstance();
+
+		// enable for rendering of text
+		GlStateManager.enableTexture();
+
+		// add addition rotation
+		GlStateManager.rotatef(rotation.w, rotation.x, rotation.y, rotation.z);
+
+		// translate to camera position
+		GlStateManager.translated(cameraTranslation.x, cameraTranslation.y, cameraTranslation.z);
+
+		// add addition rotation
+		GlStateManager.rotatef(rotation.w, -rotation.x, -rotation.y, -rotation.z);
+
+		// set up billboard rotation
+		setupBillboardRotation();
+
+		// scale text
+		GlStateManager.scalef(TEXT_SCALE, TEXT_SCALE, TEXT_SCALE);
+
+		// add billboard rotation for text readability
+		GlStateManager.rotatef(TEXT_BILLBOARD_ROTATION.w, TEXT_BILLBOARD_ROTATION.x, TEXT_BILLBOARD_ROTATION.y,
+				TEXT_BILLBOARD_ROTATION.z);
+
+		// draw
+		mc.fontRenderer.drawString(text, 0, 0, TEXT_COLOR);
+
+		resetBillboardRendering();
+	}
+
+	/**
+	 * Render a box.
+	 */
+	public static void renderBox(Vec3d translation, double dx, double dy, double dz, Vec3d pos) {
+		Tessellator tessellator = Tessellator.getInstance();
+		BufferBuilder bufferBuilder = tessellator.getBuffer();
+		bufferBuilder.setTranslation(translation.getX(), translation.getY(), translation.getZ());
+		bufferBuilder.begin(GL11.GL_LINES, DefaultVertexFormats.POSITION);
+
+		// AB
+		bufferBuilder.pos(pos.x, pos.y, pos.z).endVertex();
+		bufferBuilder.pos(pos.x, pos.y, pos.z + dz).endVertex();
+		// BC
+		bufferBuilder.pos(pos.x, pos.y, pos.z + dz).endVertex();
+		bufferBuilder.pos(pos.x + dx, pos.y, pos.z + dz).endVertex();
+		// CD
+		bufferBuilder.pos(pos.x + dx, pos.y, pos.z + dz).endVertex();
+		bufferBuilder.pos(pos.x + dx, pos.y, pos.z).endVertex();
+		// DA
+		bufferBuilder.pos(pos.x + dx, pos.y, pos.z).endVertex();
+		bufferBuilder.pos(pos.x, pos.y, pos.z).endVertex();
+		// EF
+		bufferBuilder.pos(pos.x, pos.y + dy, pos.z).endVertex();
+		bufferBuilder.pos(pos.x, pos.y + dy, pos.z + dz).endVertex();
+		// FG
+		bufferBuilder.pos(pos.x, pos.y + dy, pos.z + dz).endVertex();
+		bufferBuilder.pos(pos.x + dx, pos.y + dy, pos.z + dz).endVertex();
+		// GH
+		bufferBuilder.pos(pos.x + dx, pos.y + dy, pos.z + dz).endVertex();
+		bufferBuilder.pos(pos.x + dx, pos.y + dy, pos.z).endVertex();
+		// HE
+		bufferBuilder.pos(pos.x + dx, pos.y + dy, pos.z).endVertex();
+		bufferBuilder.pos(pos.x, pos.y + dy, pos.z).endVertex();
+		// AE
+		bufferBuilder.pos(pos.x, pos.y, pos.z).endVertex();
+		bufferBuilder.pos(pos.x, pos.y + dy, pos.z).endVertex();
+		// BF
+		bufferBuilder.pos(pos.x, pos.y, pos.z + dz).endVertex();
+		bufferBuilder.pos(pos.x, pos.y + dy, pos.z + dz).endVertex();
+		// CG
+		bufferBuilder.pos(pos.x + dx, pos.y, pos.z + dz).endVertex();
+		bufferBuilder.pos(pos.x + dx, pos.y + dy, pos.z + dz).endVertex();
+		// DH
+		bufferBuilder.pos(pos.x + dx, pos.y, pos.z).endVertex();
+		bufferBuilder.pos(pos.x + dx, pos.y + dy, pos.z).endVertex();
+
+		tessellator.draw();
+		bufferBuilder.setTranslation(0, 0, 0);
+	}
+
 }
