d5195b6cd1a2a121f5dc076da98685a53d3694f3
Thrane <einheriii@gmail.com>
2019-10-28 17:32:26 +0100

    #521

--- get-version.js
+++
@@ -1,4 +0,0 @@
-const core = require('@actions/core')
-const { modVersion, minecraftVersion } = require(process.env.GITHUB_WORKSPACE + 'version.json')
-core.setOutput('modversion', modVersion)
-core.setOutput('mcversion', minecraftVersion)
