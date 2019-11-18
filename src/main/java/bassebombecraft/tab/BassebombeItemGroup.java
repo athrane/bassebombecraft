71f48062310085d7d7897662817cd8084784fca3
Thrane <einheriii@gmail.com>
2019-11-18 14:28:41 +0100

    Sonar: Save and re-use this "Random, #573

--- src/main/java/bassebombecraft/tab/BassebombeItemGroup.java
+++ src/main/java/bassebombecraft/tab/BassebombeItemGroup.java
@@ -21,7 +21,7 @@ public class BassebombeItemGroup extends ItemGroup {
 	@Override
 	public ItemStack createIcon() {
 		Item[] items = getBassebombeCraft().getInventoryItems();
-		Random random = new Random();
+		Random random = getBassebombeCraft().getRandom();
 		int index = random.nextInt(items.length);
 		Item item = items[index];
 		return new ItemStack(item);
