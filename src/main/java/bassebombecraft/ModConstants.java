40886368d684ff2541ca27634427394782a0d658
Thrane <einheriii@gmail.com>
2020-02-10 16:56:58 +0100

    Update version info (version.json, bassebombecraft_update.json, ModConst, gradle.properties) to 1.39 #636

--- src/main/java/bassebombecraft/ModConstants.java
+++ src/main/java/bassebombecraft/ModConstants.java
@@ -57,7 +57,7 @@ public class ModConstants {
 	/**
 	 * Mod version.
 	 */
-	public static final String VERSION = "1.14.4-1.38";
+	public static final String VERSION = "1.14.4-1.39";
 
 	/**
 	 * In game tab name.
