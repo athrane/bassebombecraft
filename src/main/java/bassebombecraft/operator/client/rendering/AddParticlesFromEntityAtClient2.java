33539b7483a33a3ec05f298616333abf7407ecdc
Thrane <einheriii@gmail.com>
2020-09-21 21:43:35 +0200

    Refactor AddParticlesFromEntityAtClient2 to implement constructor with function interfaces #942

--- src/main/java/bassebombecraft/operator/client/rendering/AddParticlesFromEntityAtClient2.java
+++ src/main/java/bassebombecraft/operator/client/rendering/AddParticlesFromEntityAtClient2.java
@@ -2,6 +2,9 @@ package bassebombecraft.operator.client.rendering;
 
 import static bassebombecraft.BassebombeCraft.getProxy;
 import static bassebombecraft.event.particle.DefaultParticleRendering.getInstance;
+import static bassebombecraft.operator.DefaultPorts.getFnGetLivingEntity1;
+
+import java.util.function.Function;
 
 import bassebombecraft.event.particle.ParticleRendering;
 import bassebombecraft.event.particle.ParticleRenderingInfo;
@@ -14,7 +17,7 @@ import net.minecraft.util.math.BlockPos;
  * Implementation of the {@linkplain Operator2} interface which adds particles
  * at client side.
  * 
- * The particles are added from living entity #1 from the ports.
+ * The particles are added from the position of the living entity.
  */
 public class AddParticlesFromEntityAtClient2 implements Operator2 {
 
@@ -24,19 +27,39 @@ public class AddParticlesFromEntityAtClient2 implements Operator2 {
 	ParticleRenderingInfo[] infos;
 
 	/**
+	 * Function to get living entity.
+	 */
+	Function<Ports, LivingEntity> fnGetLivingEntity;
+
+	/**
+	 * Constructor.
+	 * 
+	 * @param fnGetEntity function to get living entity.
+	 * @param particles   particle rendering infos.
+	 */
+	public AddParticlesFromEntityAtClient2(Function<Ports, LivingEntity> fnGetLivingEntity,
+			ParticleRenderingInfo[] infos) {
+		this.fnGetLivingEntity = fnGetLivingEntity;
+		this.infos = infos;
+	}
+
+	/**
 	 * Constructor.
 	 * 
 	 * @param particles particle rendering infos.
+	 * 
+	 *                  Instance is configured with living entity #1 (at whose
+	 *                  position the particle are spawned) source from ports.
 	 */
 	public AddParticlesFromEntityAtClient2(ParticleRenderingInfo[] infos) {
-		this.infos = infos;
+		this(getFnGetLivingEntity1(), infos);
 	}
 
 	@Override
 	public Ports run(Ports ports) {
 
 		// get entity
-		LivingEntity entity = ports.getLivingEntity1();
+		LivingEntity entity = fnGetLivingEntity.apply(ports);
 		if (entity == null)
 			return ports;
 
