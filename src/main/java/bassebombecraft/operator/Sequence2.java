7c5a5e1d42fce0b28e49d8f15615605fe0267a5c
Thrane <einheriii@gmail.com>
2021-03-07 18:13:29 +0100

    Add support for resetting result state prior to execution of sequence of operators #1066

--- src/main/java/bassebombecraft/operator/Sequence2.java
+++ src/main/java/bassebombecraft/operator/Sequence2.java
@@ -12,9 +12,49 @@ import java.util.Arrays;
 public class Sequence2 implements Operator2 {
 
 	/**
+	 * Default behaviour for resetting the result state prior to execution.
+	 */
+	public static final boolean DONT_RESET = false;
+
+	/**
+	 * Default behaviour for resetting the result state prior to execution.
+	 */
+	public static final boolean RESET = false;
+
+	/**
+	 * Strategy for resetting the result state.
+	 */
+	static final Operator2 RESET_STRATEGY = new ResetResult2();
+
+	/**
+	 * Strategy for not resetting the result state.
+	 */
+	static final Operator2 DONT_RESET_STRATEGY = new NullOp2();;
+
+	/**
 	 * Embedded operators.
 	 */
 	Operator2[] operators;
+
+	/**
+	 * Reset operator.
+	 */
+	Operator2 resetOp;
+
+	/**
+	 * Constructor.
+	 * 
+	 * @param reset     if value is true then the result state in the ports will be
+	 *                  reset before execution of the operators in the sequence.
+	 *                  Useful for resetting the result state for conditional
+	 *                  execution of operators in a sequence where the ports are
+	 *                  reused across invocations.
+	 * @param operators embedded operators which are executed in sequence
+	 */
+	public Sequence2(boolean reset, Operator2... operators) {
+		resetOp = createResetStrategy(reset);
+		this.operators = operators;
+	}
 
 	/**
 	 * Constructor.
@@ -22,17 +62,31 @@ public class Sequence2 implements Operator2 {
 	 * @param operators embedded operators which are executed in sequence
 	 */
 	public Sequence2(Operator2... operators) {
-		this.operators = operators;
+		this(DONT_RESET, operators);
 	}
 
 	@Override
 	public void run(Ports ports) {
+		Operators2.run(ports, resetOp);
 		Operators2.run(ports, operators);
 	}
 
 	@Override
 	public String toString() {
 		return "Sequence2 [operator=" + Arrays.toString(operators) + "]";
+	}
+
+	/**
+	 * Create reset strategy.
+	 * 
+	 * @param reset state to create strategy from.
+	 * 
+	 * @return reset strategy.
+	 */
+	Operator2 createResetStrategy(boolean reset) {
+		if (reset)
+			return RESET_STRATEGY;
+		return DONT_RESET_STRATEGY;
 	}
 
 }
