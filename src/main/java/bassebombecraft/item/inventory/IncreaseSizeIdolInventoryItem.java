edb779fb9cd17fe486146724b34f13122c2d91ba
Thrane <einheriii@gmail.com>
2021-04-15 21:29:29 +0200

    Refactor IncreaseSizeIdolInventoryItem to operator v2 framework #1162

--- src/main/java/bassebombecraft/item/inventory/IncreaseSizeIdolInventoryItem.java
+++ src/main/java/bassebombecraft/item/inventory/IncreaseSizeIdolInventoryItem.java
@@ -3,19 +3,27 @@ package bassebombecraft.item.inventory;
 import static bassebombecraft.config.ModConfiguration.increaseSizeEffectAmplifier;
 import static bassebombecraft.config.ModConfiguration.increaseSizeEffectDuration;
 import static bassebombecraft.config.ModConfiguration.increaseSizeIdolInventoryItem;
+import static bassebombecraft.operator.DefaultPorts.getBcSetEffectInstance1;
+import static bassebombecraft.operator.DefaultPorts.getFnGetLivingEntity2;
+import static bassebombecraft.operator.DefaultPorts.getInstance;
+import static bassebombecraft.operator.LazyInitOp2.of;
 import static bassebombecraft.potion.effect.RegisteredEffects.INCREASE_SIZE_EFFECT;
 
+import java.util.function.BiConsumer;
+import java.util.function.Function;
 import java.util.function.IntSupplier;
 import java.util.function.Supplier;
 
-import bassebombecraft.item.action.inventory.ExecuteOperatorOnTarget;
-import bassebombecraft.operator.Operator;
-import bassebombecraft.operator.Operators;
-import bassebombecraft.operator.Sequence;
-import bassebombecraft.operator.conditional.IfEntityIsntType;
-import bassebombecraft.operator.entity.potion.effect.AddEffect;
-import bassebombecraft.operator.entity.potion.effect.AddEffectAtClient;
+import bassebombecraft.item.action.inventory.ExecuteOperatorOnTarget2;
+import bassebombecraft.operator.Operator2;
+import bassebombecraft.operator.Ports;
+import bassebombecraft.operator.Sequence2;
+import bassebombecraft.operator.conditional.IsEntityOfType2;
+import bassebombecraft.operator.conditional.IsNot2;
+import bassebombecraft.operator.entity.potion.effect.AddEffect2;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.potion.EffectInstance;
 
 /**
  * Increase size idol implementation.
@@ -27,18 +35,23 @@ public class IncreaseSizeIdolInventoryItem extends GenericInventoryItem {
 	static IntSupplier splDuration = () -> increaseSizeEffectDuration.get();
 	static IntSupplier splAmplifier = () -> increaseSizeEffectAmplifier.get();
 
-	static Supplier<Operators> splOp = () -> {
-		Operators ops = new Operators();
-		AddEffect addOp = new AddEffect(ops.getSplTargetEntity(), INCREASE_SIZE_EFFECT.get(), splDuration,
-				splAmplifier);
-		AddEffectAtClient addOp2 = new AddEffectAtClient(ops.getSplTargetEntity(), addOp.getSplEffectInstance());
-		Sequence seqOp = new Sequence(addOp, addOp2);
-		Operator ifOp = new IfEntityIsntType(ops.getSplTargetEntity(), seqOp, PlayerEntity.class);
-		ops.setOperator(ifOp);
-		return ops;
+	/**
+	 * Create operators.
+	 */
+	static Supplier<Operator2> splOp2 = () -> {
+		Function<Ports, LivingEntity> fnGetTarget = getFnGetLivingEntity2();
+		BiConsumer<Ports, EffectInstance> bcSetEffectInstance = getBcSetEffectInstance1();
+		return new Sequence2(new IsNot2(new IsEntityOfType2(fnGetTarget, PlayerEntity.class)),
+				new AddEffect2(fnGetTarget, bcSetEffectInstance, INCREASE_SIZE_EFFECT.get(),
+						increaseSizeEffectDuration.get(), increaseSizeEffectAmplifier.get()));
 	};
 
+	/**
+	 * Operator to setup operator initializer function for lazy initialisation.
+	 */
+	static final Operator2 lazyInitOp = of(splOp2);
+
 	public IncreaseSizeIdolInventoryItem() {
-		super(increaseSizeIdolInventoryItem, new ExecuteOperatorOnTarget(splOp.get()));
+		super(increaseSizeIdolInventoryItem, new ExecuteOperatorOnTarget2(getInstance(), lazyInitOp));
 	}
 }
