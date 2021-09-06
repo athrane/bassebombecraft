b6faca0a485c8229150f48918ddbe865d86ad8a0
Thrane <einheriii@gmail.com>
2021-09-06 23:05:54 +0200

    Increase debug output in operator LogPorts2 #1236

--- src/main/java/bassebombecraft/operator/debug/LogPorts2.java
+++ src/main/java/bassebombecraft/operator/debug/LogPorts2.java
@@ -2,6 +2,8 @@ package bassebombecraft.operator.debug;
 
 import static bassebombecraft.BassebombeCraft.getBassebombeCraft;
 
+import org.apache.logging.log4j.Logger;
+
 import bassebombecraft.operator.Operator2;
 import bassebombecraft.operator.Ports;
 
@@ -28,8 +30,25 @@ public class LogPorts2 implements Operator2 {
 
 	@Override
 	public void run(Ports ports) {
-		getBassebombeCraft().getLogger().info(header);		
-		getBassebombeCraft().getLogger().info("Result="+ports.getResult());
+		Logger logger = getBassebombeCraft().getLogger();
+		logger.info(header);		
+		logger.info("Result="+ports.getResult());
+		logger.info("Counter="+ports.getCounter());		
+		logger.info("String1="+ports.getString1());				
+		logger.info("String2="+ports.getString2());						
+		logger.info("Double1="+ports.getDouble1());								
+		logger.info("Aabb1="+ports.getAabb1());										
+		logger.info("BlockPosition1="+ports.getBlockPosition1());												
+		logger.info("BlockPosition2="+ports.getBlockPosition2());												
+		logger.info("Color4f1="+ports.getColor4f1());												
+		logger.info("Color4f2="+ports.getColor4f2());												
+		logger.info("DamageSource1="+ports.getDamageSource1());												
+		logger.info("EffectInstance1="+ports.getEffectInstance1());												
+		logger.info("Entities1="+ports.getEntities1());												
+		logger.info("Entity1="+ports.getEntity1());												
+		logger.info("Entity2="+ports.getEntity2());												
+		logger.info("LivingEntity1="+ports.getLivingEntity1());												
+		logger.info("LivingEntity2="+ports.getLivingEntity2());														
 	}
 
 }
