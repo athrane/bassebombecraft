76b0d4ccb8a59a5ab144d4f64438e2902cd338ee
Thrane <einheriii@gmail.com>
2021-09-12 21:34:52 +0200

    Update version info (version.json, bassebombecraft_update.json, ModConst, build.gradle) to 2.4 #1249

--- src/main/java/bassebombecraft/ModConstants.java
+++ src/main/java/bassebombecraft/ModConstants.java
@@ -29,7 +29,7 @@ public class ModConstants {
 	/**
 	 * Mod version.
 	 */
-	public static final String VERSION = "1.16.5-2.3";
+	public static final String VERSION = "1.16.5-2.4";
 
 	/**
 	 * In game tab name.
