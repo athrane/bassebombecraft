975e33ae9c7ccb823e119cdd2cc449919fd42bed
Thrane <einheriii@gmail.com>
2022-02-20 22:34:39 +0100

    Refactor IceBlockBook to support rendering of active combination of composite items #1307

--- src/main/java/bassebombecraft/item/book/IceBlockBook.java
+++ src/main/java/bassebombecraft/item/book/IceBlockBook.java
@@ -1,17 +1,15 @@
 package bassebombecraft.item.book;
 
 import static bassebombecraft.config.ModConfiguration.iceBlockBook;
-import static bassebombecraft.operator.DefaultPorts.getFnGetEntities1;
-import static bassebombecraft.operator.DefaultPorts.getInstance;
+import static bassebombecraft.item.RegisteredItems.FORMATION1;
+import static bassebombecraft.item.RegisteredItems.MODIFIER11;
+import static bassebombecraft.item.RegisteredItems.PROJECTILE3;
 
 import java.util.function.Supplier;
+import java.util.stream.Stream;
 
-import bassebombecraft.operator.Operator2;
-import bassebombecraft.operator.Sequence2;
-import bassebombecraft.operator.entity.raytraceresult.SpawnIceBlock2;
-import bassebombecraft.operator.projectile.ShootSkullProjectile2;
-import bassebombecraft.operator.projectile.formation.SingleProjectileFormation2;
-import bassebombecraft.operator.projectile.modifier.TagProjectileWithProjectileModifier;
+import net.minecraft.item.Item;
+import net.minecraftforge.fml.RegistryObject;
 
 /**
  * Book of ice block implementation.
@@ -21,16 +19,11 @@ public class IceBlockBook extends GenericCompositeItemsBook {
 	public static final String ITEM_NAME = IceBlockBook.class.getSimpleName();
 
 	/**
-	 * Create operators.
+	 * Composite items.
 	 */
-	static Supplier<Operator2> splOp = () -> {
-		Operator2 formationOp = new SingleProjectileFormation2();
-		Operator2 projectileOp = new ShootSkullProjectile2();
-		Operator2 modifierOp = new TagProjectileWithProjectileModifier(getFnGetEntities1(), p -> SpawnIceBlock2.NAME);
-		return new Sequence2(formationOp, projectileOp, modifierOp);
-	};
+	static Supplier<Stream<RegistryObject<Item>>> splComposites = () -> Stream.of(FORMATION1, PROJECTILE3, MODIFIER11);
 
 	public IceBlockBook() {
-		super(iceBlockBook, getInstance(), splOp.get());
+		super(iceBlockBook, splComposites);
 	}
 }
