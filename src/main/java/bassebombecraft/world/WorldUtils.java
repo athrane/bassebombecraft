6c1fabbe2d39c3d028d5678556ee6796a34d29f6
Thrane <einheriii@gmail.com>
2022-05-08 20:25:15 +0200

    The import net.minecraft.world.server cannot be resolved in class WorldUtils, closes #1385

--- src/main/java/bassebombecraft/world/WorldUtils.java
+++ src/main/java/bassebombecraft/world/WorldUtils.java
@@ -11,7 +11,6 @@ import static net.minecraft.sounds.SoundEvents.*;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.phys.Vec3;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.server.ServerWorld;
 
 /**
  * World utility class.
