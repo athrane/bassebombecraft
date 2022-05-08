adfe9eafa5182ceeaa58daf1df0fb4f65dc7408a
Thrane <einheriii@gmail.com>
2022-05-08 20:30:17 +0200

    The import net.minecraft.world.entity.projectile.LlamaSpit conflicts with a type defined in the same file, closes #1386

--- src/main/java/bassebombecraft/item/action/inventory/LlamaSpit.java
+++ src/main/java/bassebombecraft/item/action/inventory/LlamaSpit.java
@@ -8,7 +8,6 @@ import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.entity.projectile.LlamaSpit;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.util.Mth;
@@ -40,13 +39,13 @@ public class LlamaSpit implements InventoryItemActionStrategy {
 	@Override
 	public void applyEffect(LivingEntity target, Level world, LivingEntity invoker) {
 		Random random = getBassebombeCraft().getRandom();
-		LlamaSpit entity = EntityType.LLAMA_SPIT.create(world);
+		net.minecraft.world.entity.projectile.LlamaSpit entity = EntityType.LLAMA_SPIT.create(world);
 
 		// from EntityLlama.spit()
 		double d0 = target.getX() - invoker.getX();
 		double d1 = target.getBoundingBox().minY + (double) (target.getBbHeight() / 3.0F) - entity.getY();
 		double d2 = target.getZ() - invoker.getZ();
-		float f = Mth.sqrt(d0 * d0 + d2 * d2) * 0.2F;
+		float f = Mth.sqrt((float) (d0 * d0 + d2 * d2)) * 0.2F;
 		entity.shoot(d0, d1 + (double) f, d2, 1.5F, 10.0F);
 		world.playSound((Player) null, invoker.getX(), invoker.getY(), invoker.getZ(), SoundEvents.LLAMA_SPIT,
 				invoker.getSoundSource(), 1.0F, 1.0F + (random.nextFloat() - random.nextFloat()) * 0.2F);
