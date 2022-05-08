cb07294e51725b7f44d437376b6e909f68dcf8a2
Thrane <einheriii@gmail.com>
2022-05-08 20:32:37 +0200

    Type mismatch: cannot convert from DeferredRegister<Potion> to DeferredRegister<MobEffect>, closes #1387

--- src/main/java/bassebombecraft/potion/effect/RegisteredEffects.java
+++ src/main/java/bassebombecraft/potion/effect/RegisteredEffects.java
@@ -2,7 +2,7 @@ package bassebombecraft.potion.effect;
 
 import static bassebombecraft.ModConstants.MODID;
 import static net.minecraftforge.registries.DeferredRegister.create;
-import static net.minecraftforge.registries.ForgeRegistries.POTIONS;
+import static net.minecraftforge.registries.ForgeRegistries.MOB_EFFECTS;
 
 import java.util.function.Supplier;
 
@@ -18,30 +18,41 @@ public class RegisteredEffects {
 	/**
 	 * Deferred registry for registration of potion effects.
 	 */
-	public static final DeferredRegister<MobEffect> EFFECT_REGISTRY = create(POTIONS, MODID);
+	public static final DeferredRegister<MobEffect> EFFECT_REGISTRY = create(MOB_EFFECTS, MODID);
 
 	/**
 	 * Registered effects.
 	 */
 	public static final RegistryObject<MobEffect> AGGRO_MOB_EFFECT = register(AggroMobEffect.NAME, AggroMobEffect::new);
-	public static final RegistryObject<MobEffect> RECEIVE_AGGRO_EFFECT = register(ReceiveAggroEffect.NAME,ReceiveAggroEffect::new);
-	public static final RegistryObject<MobEffect> AGGRO_PLAYER_EFFECT = register(AggroPlayerEffect.NAME, AggroPlayerEffect::new);
-	public static final RegistryObject<MobEffect> MOB_PRIMING_EFFECT = register(MobPrimingEffect.NAME, MobPrimingEffect::new);
-	public static final RegistryObject<MobEffect> BACON_BAZOOKA_EFFECT = register(BaconBazookaEffect.NAME, BaconBazookaEffect::new);
-	public static final RegistryObject<MobEffect> BEAR_BLASTER_EFFECT = register(BearBlasterEffect.NAME, BearBlasterEffect::new);
-	public static final RegistryObject<MobEffect> CREEPER_CANNON_EFFECT = register(CreeperCannonEffect.NAME, CreeperCannonEffect::new);
-	public static final RegistryObject<MobEffect> PRIMED_CREEPER_CANNON_EFFECT = register(PrimedCreeperCannonEffect.NAME, PrimedCreeperCannonEffect::new);
-	public static final RegistryObject<MobEffect> AMPLIFIER_EFFECT = register(AmplifierEffect.NAME, AmplifierEffect::new);
+	public static final RegistryObject<MobEffect> RECEIVE_AGGRO_EFFECT = register(ReceiveAggroEffect.NAME,
+			ReceiveAggroEffect::new);
+	public static final RegistryObject<MobEffect> AGGRO_PLAYER_EFFECT = register(AggroPlayerEffect.NAME,
+			AggroPlayerEffect::new);
+	public static final RegistryObject<MobEffect> MOB_PRIMING_EFFECT = register(MobPrimingEffect.NAME,
+			MobPrimingEffect::new);
+	public static final RegistryObject<MobEffect> BACON_BAZOOKA_EFFECT = register(BaconBazookaEffect.NAME,
+			BaconBazookaEffect::new);
+	public static final RegistryObject<MobEffect> BEAR_BLASTER_EFFECT = register(BearBlasterEffect.NAME,
+			BearBlasterEffect::new);
+	public static final RegistryObject<MobEffect> CREEPER_CANNON_EFFECT = register(CreeperCannonEffect.NAME,
+			CreeperCannonEffect::new);
+	public static final RegistryObject<MobEffect> PRIMED_CREEPER_CANNON_EFFECT = register(
+			PrimedCreeperCannonEffect.NAME, PrimedCreeperCannonEffect::new);
+	public static final RegistryObject<MobEffect> AMPLIFIER_EFFECT = register(AmplifierEffect.NAME,
+			AmplifierEffect::new);
 	public static final RegistryObject<MobEffect> REFLECT_EFFECT = register(ReflectEffect.NAME, ReflectEffect::new);
-	public static final RegistryObject<MobEffect> INCREASE_SIZE_EFFECT = register(IncreaseSizeEffect.NAME, IncreaseSizeEffect::new);
-	public static final RegistryObject<MobEffect> DECREASE_SIZE_EFFECT = register(DecreaseSizeEffect.NAME, DecreaseSizeEffect::new);
+	public static final RegistryObject<MobEffect> INCREASE_SIZE_EFFECT = register(IncreaseSizeEffect.NAME,
+			IncreaseSizeEffect::new);
+	public static final RegistryObject<MobEffect> DECREASE_SIZE_EFFECT = register(DecreaseSizeEffect.NAME,
+			DecreaseSizeEffect::new);
 	public static final RegistryObject<MobEffect> WILDFIRE_EFFECT = register(WildfireEffect.NAME, WildfireEffect::new);
-	public static final RegistryObject<MobEffect> CONTAGION_EFFECT = register(ContagionEffect.NAME, ContagionEffect::new);
+	public static final RegistryObject<MobEffect> CONTAGION_EFFECT = register(ContagionEffect.NAME,
+			ContagionEffect::new);
 
 	/**
 	 * Register object.
 	 * 
-	 * @param key registry object name.
+	 * @param key       registry object name.
 	 * @param splEffect supplier used to create effect instance.
 	 * 
 	 * @return registry object.
@@ -49,6 +60,5 @@ public class RegisteredEffects {
 	static RegistryObject<MobEffect> register(String key, Supplier<MobEffect> splEffect) {
 		return EFFECT_REGISTRY.register(key.toLowerCase(), splEffect);
 	}
-		
-}
 
+}
