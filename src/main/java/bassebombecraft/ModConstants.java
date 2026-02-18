c898d43a7784659738219170cf8b78763f0b6b86
Allan Thrane Andersen <einheriii@gmail.com>
2026-02-18 19:59:55 +0100

    Upgrade gradleforge to 1.18.2, closes #1437

--- src/main/java/bassebombecraft/ModConstants.java
+++ src/main/java/bassebombecraft/ModConstants.java
@@ -30,7 +30,7 @@ public class ModConstants {
 	/**
 	 * Mod version.
 	 */
-	public static final String VERSION = "1.17.1-3.1";
+	public static final String VERSION = "1.18.2-4.1";
 
 	/**
 	 * In game tab name.
